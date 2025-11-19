b5ef8cbd169951b116b22950a998bd44ebc07a5a
~ 𝐔𝐍𝐍𝚲𝐓𝐈 /𝟑 𓆩💗𓆪𓈒 <222322462+BtwUnnati@users.noreply.github.com>
2025-11-19 13:11:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ankitchoudhary1200/sapna123",
+    "https://github.com/BtwUnnati/sapna123",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
